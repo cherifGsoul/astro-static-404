40bc786b12423b677afc288ad3e82429b6335d81
Cherif Bouchelaghem <cherif.bouchelaghem@gmail.com>
2024-05-11 14:19:24 -0400

    add some comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,11 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const response = await next();
+  // if there's no found page redirect to /404 route
   if (response.status === 404) {
     return context.redirect("/404")
   }
-  return next()
+
+  // otherwise we are in the happy path
+  return response;
 })
